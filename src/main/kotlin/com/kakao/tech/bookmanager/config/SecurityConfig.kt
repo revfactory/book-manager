2dfdff4de0d168ee5bb343018bf8c1d1639e65e5
robin-hwang <robin.hwang@kakaocorp.com>
2023-02-24 02:27:05 +0900

    Modify Security Config

--- src/main/kotlin/com/kakao/tech/bookmanager/config/SecurityConfig.kt
+++ src/main/kotlin/com/kakao/tech/bookmanager/config/SecurityConfig.kt
@@ -2,9 +2,7 @@ package com.kakao.tech.bookmanager.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.core.annotation.Order
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -13,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
@@ -38,6 +37,26 @@ class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    @Throws(Exception::class)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain? {
+        http
+            .authorizeHttpRequests {
+                it.requestMatchers(HttpMethod.POST, "/api/books").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/books/**").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "/api/books/**").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.POST, "/api/categories").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/api/categories/**").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.DELETE, "/api/categories/**").hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.POST, "/api/categories/**").hasRole("MANAGER")
+                    .anyRequest().hasRole("USER")
+            }
+            .formLogin()
+            .and().httpBasic()
+            .and().csrf().disable()
+        return http.build()
     }
 }
 
